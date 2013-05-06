18a6c68a62cf75eaa80a652dfcdfc26712d79faa
Vladimir Chernov <2chernov@gmail.com>
2013-05-07 00:11:00 +0300

    code of gstPluginWrap, gstFpsCount; build scripts

---
+++ plugins/gstFpsCount/FpsCount.cpp
@@ -0,0 +1,25 @@
+#include "FpsCount.h"
+
+namespace gstPluginWrap
+{
+
+void configure(Customizer*& customizer)
+{
+	customizer = new HandlerCustomizer<FpsCount>();
+}
+
+} // namespace gstPluginWrap
+
+FpsCount::FpsCount()
+{
+}
+
+FpsCount::~FpsCount()
+{
+}
+
+void FpsCount::process(uint8_t* buffer)
+{
+	if (fpsCounter.update())
+		g_print("FPS: %d\n", fpsCounter.framesPerSec());
+}
---
+++ plugins/gstFpsCount/FpsCount.h
@@ -0,0 +1,41 @@
+#ifndef FPSCOUNT_H_
+#define FPSCOUNT_H_
+
+#define GST_PLUGIN_TYPE GstFpsCount
+
+#include <timeutil/FpsCounter.h>
+
+#include <gstPluginWrap.h>
+
+namespace gstPluginWrap
+{
+
+namespace details
+{
+const char* name = "fpscount";
+const char* longName = "FPS counter";
+const char* classification = "Generic";
+const char* description = "Counts frames per second.";
+const char* originUrl = "https://github.com/vchernov/gstAssist";
+const char* author = "Vladimir Chernov <vladimir.chernov@student.uva.fi>";
+const char* license = "LGPL";
+const char* version = "0.2";
+} // namespace details
+
+const char* allowedCaps = "ANY";
+
+} // namespace gstPluginWrap
+
+class FpsCount: public gstPluginWrap::PureFrameHandler
+{
+public:
+	FpsCount();
+	virtual ~FpsCount();
+
+	void process(uint8_t* buffer);
+
+private:
+	timeutil::FpsCounter fpsCounter;
+};
+
+#endif // FPSCOUNT_H_
