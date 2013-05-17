18d49fcceed541c0e099c827f264629f9c7b8225
Vladimir Chernov <2chernov@gmail.com>
2013-05-18 00:46:21 +0300

    LGPLv3+

---
+++ gstPluginWrap/README.md
@@ -0,0 +1,24 @@
+gstPluginWrap
+=============
+
+GStreamer plug-in wrapper. Header only C++ library. Provides simple object-oriented way for plug-ins writing.
+
+
+Dependencies
+------------
+
+* [GStreamer](http://gstreamer.com/)
+
+
+Resources
+---------
+
+* Source Code: [https://github.com/vchernov/gstAssist](https://github.com/vchernov/gstAssist)
+
+
+Copying
+-------
+
+Free use of this software is granted under the terms of the GNU Lesser General Public License 
+version 3 or later (LGPLv3+). For details see the files `COPYING` and `COPYING.LESSER` included 
+with the timeutil distribution.
--- gstPluginWrap/gstPluginWrap.h
+++ gstPluginWrap/gstPluginWrap.h
@@ -1,4 +1,23 @@
 /*
+ * Copyright (C) 2013 Vladimir Chernov
+ *
+ * This file is part of gstPluginWrap library.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * Note!
  * The GST_PLUGIN_TYPE macro must be defined before inclusion of this file.
  * It is used as a name of the structure that represents GStreamer element type.
