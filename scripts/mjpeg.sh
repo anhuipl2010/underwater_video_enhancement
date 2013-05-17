472d3a127557a16451c64d6acb72208e0bf96b6a
Vladimir Chernov <2chernov@gmail.com>
2013-05-18 00:00:42 +0300

    mjpeg play script

---
+++ scripts/mjpeg.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+file=$1
+
+fps=$2
+: ${fps:=25}
+
+if (test -n "$3") && (test -n "$4"); then
+	scale="video/x-raw-yuv,width=$3,height=$4"
+else
+	scale="video/x-raw-yuv"
+fi
+
+gst-launch filesrc location=$file ! \
+multipartdemux ! jpegdec ! \
+queue ! \
+videorate ! "video/x-raw-yuv,framerate=(fraction)$fps" ! \
+queue ! \
+videoscale ! $scale ! \
+xvimagesink
