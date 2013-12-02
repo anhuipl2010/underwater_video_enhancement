9ae8b6aa9b75fcdc60ef8535519cf5f3b9ac2f2c
Vladimir Chernov <2chernov@gmail.com>
2013-12-02 14:48:23 +0200

    setenv cmd script for plugins uses pathadd

--- plugins/setenv.cmd
+++ plugins/setenv.cmd
@@ -2,11 +2,8 @@
 
 set script_dir=%~dp0
 set plugins_dir=%script_dir%bin
+set pathadd=%script_dir%..\pathadd\bin\release\pathadd.exe
 
-if ["%GST_PLUGIN_PATH%"]==[""] (
-    set GST_PLUGIN_PATH=%plugins_dir%
-) else (
-    call set GST_PLUGIN_PATH=%%GST_PLUGIN_PATH:%plugins_dir%=%%
-)
+for /f %%i in ('%pathadd% GST_PLUGIN_PATH %plugins_dir%') do set GST_PLUGIN_PATH=%%i
 
-echo GST_PLUGIN_PATH: %GST_PLUGIN_PATH%
+set GST_PLUGIN_PATH
