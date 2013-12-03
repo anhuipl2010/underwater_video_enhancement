5de1e1b10df865655181e666d881908dad2016c7
Vladimir Chernov <2chernov@gmail.com>
2013-12-04 00:40:41 +0200

    enhance script fixed (correct resolve of output file)

--- scripts/enhancement_windows/enhance.cmd
+++ scripts/enhancement_windows/enhance.cmd
@@ -1,40 +1,27 @@
 @rem Enhance underwater video and encode to H.264
 
 @echo off
-setlocal EnableDelayedExpansion
 
 set enhance_script_dir=%~dp0
 
 call %enhance_script_dir%configure.cmd
 
-set in_file=%1
-if [%in_file%]==[] (
+set in_file=%~1
+if "%in_file%"=="" (
     echo usage: %~nx0 input_file [output_file] 1>&2
-    exit /B 1
+    exit /b 1
 )
 
-set out_file=%2
-if [%out_file%]==[] (
-    if [%user_out_dir%]==[] (
-        rem directory of the input file by default
-        set out_dir=%~dp1
-    ) else (
-        set out_dir=!user_out_dir!
-    )
-
-    if [%enhancement_mode%]==[0_0] (
-        set out_file_suffix=
-    ) else (
-        set out_file_suffix=-enhanced-!enhancement_mode!
-    )
-
-    set out_file=!out_dir!!out_file_prefix!%~n1!out_file_suffix!.mp4
+set out_file=%~2
+if "%out_file%"=="" (
+    call:set_out_file "%~n1" "%~dp1" out_file
 )
 
 set temp_file=%out_file%.temp
 
 @echo on
-gst-launch-0.10 -e filesrc location=%in_file:\=/% ! ^
+gst-launch-0.10 -e ^
+filesrc location="%in_file:\=/%" ! ^
 decodebin2 ! ^
 videorate ! "video/x-raw-yuv, framerate=%fps%" ! ^
 colortuning enabled=%colortuning_enabled% cbexp=%colortuning_cbexp% crexp=%colortuning_crexp% ! ^
@@ -44,9 +31,44 @@ mp4mux ! ^
 filesink location="%temp_file:\=/%"
 @echo off
 
-if %ERRORLEVEL% neq 0 (
-	exit /B 1
+if exist "%temp_file%" (
+    for %%a in ("%temp_file%") do set temp_file_size=%%~za
+    if "%temp_file_size%" GTR "0" (
+        move /Y "%temp_file%" "%out_file%"
+        echo underwater video enhancement completed: from "%in_file%" to "%out_file%"
+    ) else (
+        del "%temp_file%"
+        echo underwater video enhancement failed: "%in_file%" 1>&2
+        exit /b 1
+    )
+) else (
+    echo underwater video enhancement failed: "%in_file%" 1>&2
+    exit /b 1
 )
 
-move /Y "%temp_file%" "%out_file%"
-echo underwater video enhancement from %in_file% to %out_file% is done
+goto:eof
+
+:set_out_file
+    setlocal EnableDelayedExpansion
+    set fn=%~1
+    set dir=%~2
+
+    if [%user_out_dir%]==[] (
+        set out_dir=%dir%
+    ) else (
+        set out_dir=%user_out_dir%
+    )
+
+    if [%enhancement_mode%]==[0_0] (
+        set out_file_suffix=
+    ) else (
+        set out_file_suffix=-enhanced-%enhancement_mode%
+    )
+
+    set ret_val=!out_dir!!out_file_prefix!!fn!!out_file_suffix!.mp4
+
+    (
+        endlocal
+        set %~3=%ret_val%
+    )
+goto:eof
